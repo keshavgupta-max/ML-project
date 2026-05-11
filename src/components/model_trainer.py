e44f5d08cafbfd86a90bbac45fd51e0c57640ff7
keshav <keshavguptare@gmail.com>
2026-05-11 21:04:31 +0530

    data ingestion

---
+++ src/components/model_trainer.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+import os
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
